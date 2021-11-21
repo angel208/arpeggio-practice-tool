d246b2af97182ce49a7816d7d92e31869e9de9d4
angel fabricio peña <angel.fp204@gmail.com>
2021-11-21 15:34:48 -0300

    initial commit

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <nav>
+            <p>Hecho por mi</p>
+        </nav>
+    )
+}
---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Culpa anim duis nostrud ullamco est aliqua qui nostrud duis magna. Ea nisi voluptate elit proident. Adipisicing ullamco voluptate velit enim. Ipsum cillum laborum ipsum eu aliquip veniam. Culpa aliqua officia sint nulla excepteur aliqua consequat minim sunt minim mollit mollit ex.</p>
+        </div>
+    )
+}
