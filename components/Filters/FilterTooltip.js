3feb0464a1cd961c0e5936bab3a28e6618257a61
angel fabricio peña <angel.fp204@gmail.com>
2021-12-26 20:36:15 -0300

    responsiveness fix

--- components/Filters/FilterTooltip.js
+++ components/Filters/FilterTooltip.js
@@ -4,7 +4,7 @@ import { CgInfo } from 'react-icons/cg';
 
 function FilterTooltip({ text='placeholder', size='sm'}) {
     return (
-        <Tooltip label={text} placement='right' size={size} py={2}>
+        <Tooltip label={text} placement={'auto'} size={size} py={2}>
             <Box display={'inline-block'} color={'gray.500'} mx={3} cursor={'pointer'} ><CgInfo size={16}/></Box>
         </Tooltip>
     )
