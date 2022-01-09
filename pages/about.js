eb2b2d6505381ca875921b64b9276e34cf88fb27
angel fabricio peña <angel.fp204@gmail.com>
2022-01-08 22:24:19 -0300

    about content

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Container, Heading, Text } from '@chakra-ui/react'
+import { Container, Heading, Text, VStack } from '@chakra-ui/react'
 
 export default function About() {
     return (
-        <Container h={'90vh'} maxWidth={'90%'} py={10}>
+        <VStack alignItems={'left'}  h={'90vh'} maxWidth={'100%'} px={10} py={10} spacing={5}>
             <Heading>About</Heading>
-            <Text>Culpa anim duis nostrud ullamco est aliqua qui nostrud duis magna. Ea nisi voluptate elit proident. Adipisicing ullamco voluptate velit enim. Ipsum cillum laborum ipsum eu aliquip veniam. Culpa aliqua officia sint nulla excepteur aliqua consequat minim sunt minim mollit mollit ex.</Text>
-        </Container>
+            <Text>This online tool was created to help in the arpeggio learning and practice process on the guitar. The idea is pretty simple: the tool will randomly play an arpeggio and it will display a diagram with its intervals and position on the fretboard. This way you can play random arpeggios on your guitar, helping you in the process of memorizing them.</Text>
+            <Text>The arpeggios included in this tool can have its roots either in the 6th or the 5th string, and can start with the index, middle or pinky finger. You can also filter which chord types do you want to practice and which fingers and strings do you want the arpeggios to start in. You can even select individual chords, and they will appear randomly in the arpeggio player.</Text>
+            <Text>This tool is mostly based on my personal practice routine, but I hope you can find it useful.</Text>
+        </VStack>
     )
 }
